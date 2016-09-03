a2fae96b8e9e72b9009c9e8cf78a524f0021e096
sw00 <sett.wai@gmail.com>
2016-09-03 19:04:11 +0300

    Refactor pangram.py to use sets

--- python/pangram/pangram.py
+++ python/pangram/pangram.py
@@ -3,12 +3,4 @@ import string
 ALPHABET = list(string.ascii_lowercase)
 
 def is_pangram(sentence):
-    if len(sentence) is 0:
-        return False
-
-
-    for a in ALPHABET:
-        if not (a in str.lower(sentence)):
-            return False
-
-    return True
+    return set(sentence.lower()).issuperset(set(ALPHABET))
