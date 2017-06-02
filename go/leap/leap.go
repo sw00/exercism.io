ab3cb127ed32efa39059297f210c943d7f496b02
sw00 <sett.wai@gmail.com>
2017-06-02 11:53:12 +0200

    refactor leap.go

--- go/leap/leap.go
+++ go/leap/leap.go
@@ -6,11 +6,5 @@ const testVersion = 3
 // IsLeapYear determines whether a given year (int) is
 // a leap year.
 func IsLeapYear(year int) bool {
-	// Write some code here to pass the test suite.
-	if year%4 == 0 && year%100 > 0 {
-		return true
-	} else if year%400 == 0 {
-		return true
-	}
-	return false
+	return (year%4 == 0 && year%100 > 0) || (year%400 == 0)
 }
